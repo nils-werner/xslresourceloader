6cd0636f029854fa37237fb4cdd1a24e5ecad71f
Nils Werner <nils.werner@gmail.com>
2012-06-29 21:00:54 +0200

    Extension renamed and stripped from unneccesary stuff

--- extension.driver.php
+++ extension.driver.php
@@ -2,15 +2,10 @@
 
 	require_once(TOOLKIT . '/class.datasourcemanager.php');
 
-	class Extension_GlobalResourceLoader extends Extension {
+	class Extension_ResourcesInXSL extends Extension {
 	/*-------------------------------------------------------------------------
 		Definition:
 	-------------------------------------------------------------------------*/
-
-		public function uninstall() {
-			Symphony::Configuration()->remove('globalresourceloader');
-			return Symphony::Configuration()->write();
-		}
 
 		public function getSubscribedDelegates() {
 			return array(
@@ -18,21 +13,6 @@
 					'page'		=> '/frontend/',
 					'delegate'	=> 'FrontendPageResolved',
 					'callback'	=> 'manipulatePageData'
-				),
-				array(
-					'page'		=> '/system/preferences/',
-					'delegate'	=> 'AddCustomPreferenceFieldsets',
-					'callback'	=> 'preferences'
-				)
-			);
-		}
-
-		public function fetchNavigation() {
-			return array(
-				array(
-					'location'	=> __('Blueprints'),
-					'name'		=> __('Global Resources'),
-					'link'		=> '/preferences/'
 				)
 			);
 		}
@@ -56,32 +36,10 @@
 
 		// Apply --------------------------------------------------------------
 
-			// Ignore excluded pages:
-			if (!$this->isDSPageSelected($context['page_data']['id'])) {
-				$context['page_data']['data_sources'] = $datasources;
-			}
-
-			if (!$this->isEventPageSelected($context['page_data']['id'])) {
-				$context['page_data']['events'] = $events;
-			}
+			$context['page_data']['data_sources'] = $datasources;
+			$context['page_data']['events'] = $events;
 		}
 
-	/*-------------------------------------------------------------------------
-		Utilities:
-	-------------------------------------------------------------------------*/
-
-		public function setConfig($name, $values) {
-			if (is_array($values)) {
-				$values = implode(',', $values);
-				Symphony::Configuration()->set($name, $values, 'globalresourceloader');
-			}
-
-			else {
-				Symphony::Configuration()->remove($name, 'globalresourceloader');
-			}
-
-			return Symphony::Configuration()->write();
-		}
 
 	/*-------------------------------------------------------------------------
 		Events:
@@ -89,30 +47,6 @@
 
 		public function getEventNames() {
 			return explode(',', Symphony::Configuration()->get('event-names', 'globalresourceloader'));
-		}
-
-		public function getEventPages() {
-			return explode(',', Symphony::Configuration()->get('event-pages', 'globalresourceloader'));
-		}
-
-		public function setEventNames($values) {
-			$this->setConfig('event-names', $values);
-		}
-
-		public function setEventPages($values) {
-			$this->setConfig('event-pages', $values);
-		}
-
-		public function isEventNameSelected($handle) {
-			$datasources = $this->getEventNames();
-
-			return in_array($handle, $datasources);
-		}
-
-		public function isEventPageSelected($id) {
-			$pages = $this->getEventPages();
-
-			return in_array($id, $pages);
 		}
 
 	/*-------------------------------------------------------------------------
@@ -123,27 +57,4 @@
 			return explode(',', Symphony::Configuration()->get('ds-names', 'globalresourceloader'));
 		}
 
-		public function getDSPages() {
-			return explode(',', Symphony::Configuration()->get('ds-pages', 'globalresourceloader'));
-		}
-
-		public function setDSNames($values) {
-			$this->setConfig('ds-names', $values);
-		}
-
-		public function setDSPages($values) {
-			$this->setConfig('ds-pages', $values);
-		}
-
-		public function isDSNameSelected($handle) {
-			$datasources = $this->getDSNames();
-
-			return in_array($handle, $datasources);
-		}
-
-		public function isDSPageSelected($id) {
-			$pages = $this->getDSPages();
-
-			return in_array($id, $pages);
-		}
 	}
